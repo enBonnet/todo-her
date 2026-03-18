57399284b2a54eb8cbc73372ac2bacc90fe9a547
edrick <edrickshl@gmail.com>
2026-03-17 21:16:35 -0300

    agg el put aun no se si funciona

--- src/routes/api/todos/+server.js
+++ src/routes/api/todos/+server.js
@@ -1,5 +1,6 @@
 import { db } from '$lib/server/db'
 import { task } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 // get *default* https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/GET
 // return data or get data from the server
@@ -29,8 +30,31 @@ export const POST = async ({ request }) => {
 // update https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/PUT
 // get parameters in the url mostly query search params
 // receives data in the body
-export const PUT = () => {
-    return new Response(JSON.stringify({ message: 'Hello Update' }));
+/** @type {import('./$types').RequestHandler} */
+export const PUT = async ({ request, url }) => {
+    const taskId = url.searchParams.get('id');
+    
+    if (!taskId) {
+        return new Response(JSON.stringify({ error: 'Task ID is required' }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
+    const data = await request.json();
+    
+    const updatedTask = await db.update(task)
+        .set({
+            title: data.title,
+            description: data.description,
+            priority: data.priority
+        })
+        .where(eq(task.id, taskId))
+        .returning();
+    
+    return new Response(JSON.stringify({ message: 'Task updated', task: updatedTask }), {
+        headers: { 'Content-Type': 'application/json' }
+    });
 }
 
 // delete https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/DELETE
